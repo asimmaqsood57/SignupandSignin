c16c608b815367a2875f8a6084029625981307e7
M.Asim <asimmaqsood57@gmail.com>
2021-04-14 08:31:27 +0500

    All are done and there is no bug yet

---
+++ Sign in/index.js
@@ -0,0 +1,15 @@
+// let inputEmail = document.getElementById('email');
+// let inputPass = document.getElementById('password');
+// inputEmail.addEventListener('click',inputField);
+// inputPass.addEventListener('click', inputField);
+// function inputField(){
+
+// inputEmail.style.backgroundColor = "transparent";
+// inputEmail.style.color = "white";
+// inputEmail.style.outline = "none";
+// inputPass.style.backgroundColor = "transparent";
+// inputPass.style.color = "white";
+// inputPass.style.outline = "none";
+
+
+// }
